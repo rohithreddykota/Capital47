e83aa1316751be095742574f9683d6355ce438bd
anushareddy5 <anushareddydental@gmail.com>
2023-11-20 10:06:59 -0500

    reset ui

--- web/src/pages/Dashboard/Layout/components/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as MoneyAvailable } from './MoneyAvailable/MoneyAvailable';
-export { default as Conversions } from './Conversions/Conversions';
-export { default as ChoosePlan } from './ChoosePlan/ChoosePlan';
-export { default as PaymentDetails } from './PaymentDetails/PaymentDetails';
